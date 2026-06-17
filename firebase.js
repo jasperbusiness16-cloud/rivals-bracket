e0df8a9e0dfd513aede48dd1f2a975c6333bc41f
jasperbusiness16-cloud <jasperbusiness16@gmail.com>
2026-06-17 17:25:51 -0600

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDKEp-XP1QDCvnAJjn_tGkvjg2nMveJdq8",
+  authDomain: "rivals-bracket-eb713.firebaseapp.com",
+  databaseURL: "https://rivals-bracket-eb713-default-rtdb.firebaseio.com",
+  projectId: "rivals-bracket-eb713",
+  storageBucket: "rivals-bracket-eb713.firebasestorage.app",
+  messagingSenderId: "248737838095",
+  appId: "1:248737838095:web:583b27b79021b8ac9c9f7a"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();

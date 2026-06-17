984b69ec20e31381eafc4d8cb3167446f062a94c
jasperbusiness16-cloud <jasperbusiness16@gmail.com>
2026-06-17 15:13:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,73 @@
-function getLoser(winner, teamA, teamB) {
-  if (winner === teamA) return teamB;
-  if (winner === teamB) return teamA;
-  return "";
-}
-
-function setOptions(selectId, teamA, teamB, label) {
-  const select = document.getElementById(selectId);
-  select.innerHTML = "";
-
-  let defaultOption = document.createElement("option");
-  defaultOption.text = label;
-  select.add(defaultOption);
-
-  if (teamA) {
-    let optionA = document.createElement("option");
-    optionA.text = teamA;
-    select.add(optionA);
+function pickMatch(match, winner, loser) {
+  if (match === 1) {
+    document.getElementById("semiA1").innerText = winner;
+    document.getElementById("loserA1").innerText = loser;
   }
 
-  if (teamB) {
-    let optionB = document.createElement("option");
-    optionB.text = teamB;
-    select.add(optionB);
+  if (match === 2) {
+    document.getElementById("semiA2").innerText = winner;
+    document.getElementById("loserA2").innerText = loser;
   }
-}
 
-function match1() {
-  const winner = document.getElementById("m1").value;
-  const loser = getLoser(winner, "Team 1", "Team 2");
-  document.getElementById("la1").innerText = loser;
+  if (match === 3) {
+    document.getElementById("semiB1").innerText = winner;
+    document.getElementById("loserB1").innerText = loser;
+  }
 
-  const semi = document.getElementById("semiA");
-  setOptions("semiA", winner, semi.options[2]?.text || "", "Group A Final");
+  if (match === 4) {
+    document.getElementById("semiB2").innerText = winner;
+    document.getElementById("loserB2").innerText = loser;
+  }
+
   saveBracket();
 }
 
-function match2() {
-  const winner = document.getElementById("m2").value;
-  const loser = getLoser(winner, "Team 3", "Team 4");
-  document.getElementById("la2").innerText = loser;
-
-  const semi = document.getElementById("semiA");
-  setOptions("semiA", semi.options[1]?.text || "", winner, "Group A Final");
+document.getElementById("semiA1").onclick = function() {
+  document.getElementById("finalA").innerText = this.innerText;
   saveBracket();
-}
+};
 
-function match3() {
-  const winner = document.getElementById("m3").value;
-  const loser = getLoser(winner, "Team 5", "Team 6");
-  document.getElementById("lb1").innerText = loser;
-
-  const semi = document.getElementById("semiB");
-  setOptions("semiB", winner, semi.options[2]?.text || "", "Group B Final");
+document.getElementById("semiA2").onclick = function() {
+  document.getElementById("finalA").innerText = this.innerText;
   saveBracket();
-}
+};
 
-function match4() {
-  const winner = document.getElementById("m4").value;
-  const loser = getLoser(winner, "Team 7", "Team 8");
-  document.getElementById("lb2").innerText = loser;
-
-  const semi = document.getElementById("semiB");
-  setOptions("semiB", semi.options[1]?.text || "", winner, "Group B Final");
+document.getElementById("semiB1").onclick = function() {
+  document.getElementById("finalB").innerText = this.innerText;
   saveBracket();
-}
+};
 
-function semiA() {
-  const winner = document.getElementById("semiA").value;
-  const final = document.getElementById("final");
-  setOptions("final", winner, final.options[2]?.text || "", "BO3 Finale");
+document.getElementById("semiB2").onclick = function() {
+  document.getElementById("finalB").innerText = this.innerText;
   saveBracket();
-}
+};
 
-function semiB() {
-  const winner = document.getElementById("semiB").value;
-  const final = document.getElementById("final");
-  setOptions("final", final.options[1]?.text || "", winner, "BO3 Finale");
+document.getElementById("finalA").onclick = function() {
+  document.getElementById("champion").innerText = "🏆 " + this.innerText;
   saveBracket();
-}
+};
 
-function finalWinner() {
-  const winner = document.getElementById("final").value;
-  document.getElementById("champion").innerText = "🏆 " + winner;
+document.getElementById("finalB").onclick = function() {
+  document.getElementById("champion").innerText = "🏆 " + this.innerText;
   saveBracket();
-}
+};
 
 function saveBracket() {
   const data = {};
-  document.querySelectorAll("select, .slot, .champion").forEach(el => {
-    data[el.id] = el.value || el.innerText;
+  document.querySelectorAll(".team, .champion").forEach(el => {
+    data[el.id || el.className] = el.innerText;
   });
   localStorage.setItem("rivalsBracket", JSON.stringify(data));
 }
+
+function loadBracket() {
+  const saved = JSON.parse(localStorage.getItem("rivalsBracket"));
+  if (!saved) return;
+
+  document.querySelectorAll(".team, .champion").forEach(el => {
+    const key = el.id || el.className;
+    if (saved[key]) el.innerText = saved[key];
+  });
+}
+
+loadBracket();

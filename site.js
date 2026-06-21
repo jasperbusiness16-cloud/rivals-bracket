d799582f3745a5f78a4cbaccbc6d74b92c83b73a
jasperbusiness16-cloud <jasperbusiness16@gmail.com>
2026-06-20 22:38:20 -0600

    Update site.js

--- site.js
+++ site.js
@@ -1,1 +1,27 @@
+const siteRef = database.ref("site");
 
+siteRef.on("value", (snapshot) => {
+  const data = snapshot.val();
+
+  if (!data) return;
+
+  document.querySelectorAll("[data-event-name]").forEach(el => {
+    el.innerText = data.eventName || "";
+  });
+
+  document.querySelectorAll("[data-status]").forEach(el => {
+    el.innerText = data.status || "";
+  });
+
+  document.querySelectorAll("[data-next-event]").forEach(el => {
+    el.innerText = data.nextEvent || "";
+  });
+
+  document.querySelectorAll("[data-current-match]").forEach(el => {
+    el.innerText = data.currentMatch || "";
+  });
+
+  document.querySelectorAll("[data-format]").forEach(el => {
+    el.innerText = data.format || "";
+  });
+});

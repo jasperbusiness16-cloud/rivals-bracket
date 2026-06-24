9ebc928492efd02f7419f8c6f32de1c2feb00100
jasperbusiness16-cloud <jasperbusiness16@gmail.com>
2026-06-24 01:33:37 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -225,6 +225,7 @@ const team16 = document.getElementById("team16").value;
     formatType: document.getElementById("formatType").value,
 
     team1, team2, team3, team4, team5, team6, team7, team8,
+team9, team10, team11, team12, team13, team14, team15, team16,
 
     qf1Winner,
     qf2Winner,
